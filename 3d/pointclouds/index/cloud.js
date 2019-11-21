4efed8ee540794bf7d88fc14ecb997a6374b0894
tastyfreeze <boyd@townbranch.org>
2019-11-21 16:58:21 -0500

    add potree map

---
+++ 3d/pointclouds/index/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6140195,
+    "boundingBox": {
+        "lx": 5671952.26,
+        "ly": 4030727.45,
+        "lz": 683.26,
+        "ux": 5675952.25,
+        "uy": 4034727.4400000006,
+        "uz": 4683.250000000224
+    },
+    "tightBoundingBox": {
+        "lx": 5671952.26,
+        "ly": 4030727.45,
+        "lz": 683.26,
+        "ux": 5675952.25,
+        "uy": 4034727.44,
+        "uz": 1150.19
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 34.64093017578125,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
